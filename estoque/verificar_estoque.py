7c0235dc0c396268d3ebb9e828aa688cbb9be4e7
pbarbosa30 <167947898+pbarbosa30@users.noreply.github.com>
2024-06-09 08:37:35 -0300

    Update verificar_estoque.py

--- estoque/verificar_estoque.py
+++ estoque/verificar_estoque.py
@@ -1,9 +1,1 @@
-def verificar_estoque(tipo_sanguineo):
-    estoque = 0
-    # Ler o arquivo de bolsas de sangue
-    with open('bolsas.txt', 'r') as file:
-        for linha in file:
-            bolsa = eval(linha.strip())
-            if bolsa['tipo_sanguineo'] == tipo_sanguineo:
-                estoque += bolsa['quantidade']
-    return estoque
+
