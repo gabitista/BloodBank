ce0796ab86d512479e3a1a6cd35284734b6932b2
pbarbosa30 <167947898+pbarbosa30@users.noreply.github.com>
2024-06-09 10:02:11 -0300

    Update cadastro_receptor.py

--- receptores/cadastro_receptor.py
+++ receptores/cadastro_receptor.py
@@ -1,3 +1,5 @@
+import json
+
 def cadastrar_receptor(nome, idade, tipo_sanguineo, cpf):
     receptor = {
         'nome': nome,
@@ -5,6 +7,21 @@ def cadastrar_receptor(nome, idade, tipo_sanguineo, cpf):
         'tipo_sanguineo': tipo_sanguineo,
         'cpf': cpf
     }
-    with open('receptores.txt', 'a') as file:
-        file.write(f"{receptor}\n")
+    with open('receptores.json', 'a') as file:
+        json.dump(receptor, file)
+        file.write('\n')
     return receptor
+
+def main():
+    print("Cadastro de Receptores de Bolsa de Sangue")
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    tipo_sanguineo = input("Tipo Sanguíneo: ")
+    cpf = input("CPF: ")
+    
+    receptor_cadastrado = cadastrar_receptor(nome, idade, tipo_sanguineo, cpf)
+    print("Receptor cadastrado com sucesso:")
+    print(receptor_cadastrado)
+
+if __name__ == "__main__":
+    main()
