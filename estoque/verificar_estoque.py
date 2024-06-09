a5f3fa1a3ea40ebf0df72adef6be9aa80b7087a1
GeTurim <168144962+GeTurim@users.noreply.github.com>
2024-06-09 13:15:35 -0300

    Update verificar_estoque.py
    Coloquei minha parte

--- estoque/verificar_estoque.py
+++ estoque/verificar_estoque.py
@@ -1,1 +1,9 @@
-
+def verificar_estoque(tipo_sanguineo):
+    estoque = 0
+    # Ler o arquivo de bolsas de sangue
+    with open('bolsas.txt', 'r') as file:
+        for linha in file:
+            bolsa = eval(linha.strip())
+            if bolsa['tipo_sanguineo'] == tipo_sanguineo:
+                estoque += bolsa['quantidade']
+    return estoque
